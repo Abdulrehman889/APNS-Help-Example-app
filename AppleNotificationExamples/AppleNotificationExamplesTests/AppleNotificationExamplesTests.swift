2a5bdf8ea20f053172713a7fb2eddb95792661e6
Abdulrehman889 <98525404+Abdulrehman889@users.noreply.github.com>
2024-10-01 19:27:58 +0500

    -- init project apple notificaitons

---
+++ AppleNotificationExamples/AppleNotificationExamplesTests/AppleNotificationExamplesTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppleNotificationExamplesTests.swift
+//  AppleNotificationExamplesTests
+//
+//  Created by Muhammad Abdulrehman on 01/10/2024.
+//
+
+import Testing
+@testable import AppleNotificationExamples
+
+struct AppleNotificationExamplesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
